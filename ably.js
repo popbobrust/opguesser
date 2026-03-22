d27d2da606bb847e92a5ea277a790654d9f0be29
popbobrust <popbob.rust@gmail.com>
2026-03-22 01:00:37 -0500

    Update ably.js

--- ably.js
+++ ably.js
@@ -1,5 +1,5 @@
 // Replace with your Ably API key
-const ABLY_API_KEY = "YOUR_ABLY_API_KEY_HERE";
+const ABLY_API_KEY = "Tc6tgg.2AaD1g:343cd6ZXTJBHgZy3mH7m0JWieoWPjRrKX18E-BF1ZBQ";
 
 const ably = new Ably.Realtime(ABLY_API_KEY);
 
