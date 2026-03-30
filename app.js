b744a6ae89650dd3b799b89a01dad0ac5bcb4649
popbobrust <popbob.rust@gmail.com>
2026-03-30 11:38:39 -0500

    Update app.js

--- app.js
+++ app.js
@@ -141,7 +141,10 @@ function renderOperators() {
 
     const metaDiv = document.createElement("div");
     metaDiv.className = "operator-meta";
-    metaDiv.textContent = `${op.side} • ${op.speed}-speed / ${op.armor}-armor • ${op.season}`;
+    metaDiv.textContent =
+  `${op.side} • ${op.role} • ${op.speed}-speed / ${op.armor}-armor • ` +
+  `${op.uniqueWeapon ? "Unique Weapon" : "No Unique Weapon"} • ${op.season}`;
+
 
     card.appendChild(nameDiv);
     card.appendChild(metaDiv);
